df8cfc75838c9a0bf5e9215a4acd912659433ab6
Riveascore <agaxx010@morris.umn.edu>
2013-11-11 22:03:47 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NATEA::Application.config.secret_key_base = '118e1ca2e01f872734e57bbff50dce40514c918380ac5a23bac9623c2fcc2742d2078f5f409868359ca9f38f734a3b9310f8361dc8a2b094ebb76e984d1382d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NATEA::Application.config.session_store :cookie_store, key: '_NATEA_session'
